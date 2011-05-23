9e37dd854ca6e358a770198e3938c65f548ab76a
Rachel Hogue <hoguer711@gmail.com>
2011-05-23 13:54:19 -0400

    updates

--- clean_files/clean_TLG.py
+++ clean_files/clean_TLG.py
@@ -4,19 +4,21 @@ def strip_unwanted_chars(file):
     f=open(file, "r")
     s=f.read()
 
-    #removes all characters with value greater than 127
-    s = "".join(i for i in s if ord(i)<128)
-
     #Put spaces around the characters [. , : ; _] so that they are treated as individual words.
     s = re.sub(r"([.,:;_])", r" \1 ", s)
 
     #replaces brackets {} and [] and any enclosed text with a space.
     #Also, any following integers, 123456789, have been removed, as TLG sometimes uses [#.....]# or {#.....}# to indicate a certain style of bracket.
-    s = re.sub(r"\{[^}]*\}[0-9]+", " ", s)
-    s = re.sub(r"\[[^}]*\][0-9]+", " ", s)
+    s = re.sub(r"\{[^}]*\}[0-9]*", " ", s)
+    s = re.sub(r"\[[^}]*\][0-9]*", " ", s)
 
     #replaces the characters [@ %] and any immediately following integers with a space
-    s = re.sub(r"[@|%][0-9]+", " ", s)
+    s = re.sub(r"[@|%][0-9]*", " ", s)
+
+    #removes all characters that are not specified on this page: http://www.tlg.uci.edu/encoding/
+    keep= "ABGDEZHQIKLMNCOPRSTUFXYW= ()\\.,;:\-|/_\t\n\r\f\v"
+    s = "".join(i for i in s if i in keep)
+                
 
     #replace multiple whitespaces with one space
     s = re.sub(r"[\s]+", " ", s)
